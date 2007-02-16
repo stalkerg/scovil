2de170c8abd390c5410322bc0152c1f7b2e75174
stalkerg <stalkerg@02b043cb-f327-0410-be5f-3fa6dc1791a0>
2007-02-16 23:39:29 +0000

    add new class.

---
+++ trunk/src/object_dot.cpp
@@ -0,0 +1,9 @@
+// Object class cppfile
+#include <iostream>
+#include "object_dot.h"
+
+void object_dot::draw()
+	{
+	std::cout<<"object_dot draw()"<<std::endl;
+	}
+
---
+++ trunk/src/object_dot.h
@@ -0,0 +1,14 @@
+#ifndef OBJECT_DOT
+#define OBJECT_DOT
+
+#include "object.h"
+#include <list>
+
+class object_dot: public object
+	{
+	public:
+	
+	virtual void draw();
+	};
+
+#endif
