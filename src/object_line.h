d3f34f5466183ae670006885c8b08aec0a0575fa
linpokemon <linpokemon@02b043cb-f327-0410-be5f-3fa6dc1791a0>
2007-03-10 21:32:17 +0000

    добавил object_line для отрисовки линии и изменил exemple1.cpp

---
+++ src/object_line.h
@@ -0,0 +1,18 @@
+#ifndef OBJECT_LINE
+#define OBJECT_LINE
+
+#include "object.h"
+
+
+
+class object_line: public object
+	{
+	
+	public:
+	
+	virtual void draw();
+	
+	void draw_line();
+	};
+
+#endif
