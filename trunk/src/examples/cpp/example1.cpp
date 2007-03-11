f0cf2c9493cc9411070c5579f262d8ec9b5403ce
linpokemon <linpokemon@02b043cb-f327-0410-be5f-3fa6dc1791a0>
2007-03-11 00:42:35 +0000

    Тестировал работоспособность списков в классах object_dot и object_line, к тому несколько увеличил размер точки в классе object_dot

--- trunk/src/examples/cpp/example1.cpp
+++ trunk/src/examples/cpp/example1.cpp
@@ -10,20 +10,45 @@ int main()
 set_options(640, 480, 32, true, false);
 set_caption("Example1", "Test Example 1");
 
+
+
+
 object *first_object = new object_scene;
-set_head_object(first_object);
+set_head_object(first_object);//делаем глазки :)
 object_dot *dot1 = new object_dot;
-dot1->cord.x = 0;
-dot1->cord.y = 0;
-dot1->cord.z = -1;
+dot1->cord.x = -0.2;
+dot1->cord.y = 0.7;
+dot1->cord.z = 0.9;
 first_object->add_object(dot1);
 
+object_dot *dot2 = new object_dot;
+dot2->cord.x = 0.2;
+dot2->cord.y = 0.7;
+dot2->cord.z = 0.9;
+first_object->add_object(dot2);
+
 object *second_object = new object_scene;
-set_head_object(second_object);
+//set_head_object(second_object);//делаем ротик :)
 object_line *line1 = new object_line;
+line1->cord.x = 0;
+line1->cord.y = 0;
+line1->cord.z = 0;
+line1->cord2.x =1;
+line1->cord2.y =1;
+line1->cord2.z =3;
 second_object->add_object(line1);
+
+object_line *line2 = new object_line;
+line2->cord.x = 0;
+line2->cord.y = 0;
+line2->cord.z = 0;
+line2->cord2.x =-1;
+line2->cord2.y =1;
+line2->cord2.z =3;
+second_object->add_object(line2);
 
 SDL_Thread *cyle_thread = init_cycle();
 SDL_WaitThread(cyle_thread, NULL);
+
 return 0;
 }
--- trunk/src/object_line.cpp
+++ trunk/src/object_line.cpp
@@ -7,31 +7,39 @@ void object_line::draw()
 	#ifdef DEBUG
 	std::cout<<"object_line draw()"<<std::endl;
 	#endif
+	if(lower_objects.size())
+		{
+		draw_all();
+		}
+	else
+		{
+		draw_body();
+		}
 	
-	draw_line();
 	}
 	
-void object_line::draw_line()
+void object_line::draw_body()
 	{
 	
-/*
-  glBegin (GL_LINES);
-  for (float xtmp = -1.0F; float xtmp < 1.0F;float xtmp += 0.05)
-    {
-      glVertex2f (xtmp, -1.0);
-      glVertex2f (xtmp, 1.0);
-      glVertex2f (-1.0, xtmp);
-      glVertex2f (1.0, xtmp);
-    };
-  glEnd ();
-*/
-glBegin (GL_LINES);
-  glVertex2f (-1.0, 0.0);
-  glVertex2f (1.0, 0.0);
-  glVertex2f (0.0, -1.0);
-  glVertex2f (0.0, 1.0);
-  glEnd ();
-*/
+	glBegin (GL_LINES);
+  	glVertex3f (cord.x, cord.y,cord.z);
+  	glVertex3f (cord2.x,cord2.y ,cord2.z);
+  	glEnd ();
+
 	}
 
-	
+void object_line::draw_all()
+	{
+	std::list<object_container>::iterator object_iterator;
+	glPushMatrix();
+		glTranslatef(mat_cord.x, mat_cord.y,mat_cord.z);
+		glTranslatef(mat_cord2.x, mat_cord2.y,mat_cord2.z);
+		draw_body();
+
+		for(object_iterator=lower_objects.begin();
+		    object_iterator==lower_objects.end();
+		    ++object_iterator)
+			object_iterator->p_object->draw();
+	glPopMatrix();
+	}
+
