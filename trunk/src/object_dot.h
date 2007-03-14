1f032de8519e29ef19b4fa24d78ad8b34386556d
linpokemon <linpokemon@02b043cb-f327-0410-be5f-3fa6dc1791a0>
2007-03-14 19:35:48 +0000

    add functions: set_cord; set_mat_cord; set_color; set_size;

--- trunk/src/object_dot.h
+++ trunk/src/object_dot.h
@@ -23,12 +23,19 @@ class object_dot: public object
 
 class  object_change_dot:public object_change
 	{
-	public:
-	object_change_dot();
-	object_dot *body_object_dot;
-	vec3 cord, mat_cord;
+	vec3 cord;
+	vec3 mat_cord;
 	color4 color;
 	GLint size;
+	
+	public:
+	
+	object_change_dot();
+	object_dot *body_object_dot;
+	void set_cord(vec3 in_cord);
+	void set_mat_cord (vec3 in_mat_cord);
+	void set_color(color4 in_color);
+	void set_size(GLint in_size);
 	bool cord_change, mat_cord_change, color_change, size_change;
 	void apple(object_dot *in_dot);
 	};
