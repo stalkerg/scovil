13e918a82a8b6cbcd1c5b41a9a67b42bd6a7f072
linpokemon <linpokemon@02b043cb-f327-0410-be5f-3fa6dc1791a0>
2007-03-19 23:13:27 +0000

    small change...

--- src/object_line.h
+++ src/object_line.h
@@ -20,5 +20,39 @@ namespace scovil
 		void draw_body();
 		void draw_all();
 		};
-	}
+	
+	class  object_change_line:public object_change
+		{
+		vec3 start;
+		vec3 end;
+		vec3 mat_cord;
+		color4 start_color;
+		color4 end_color;
+		GLint width;
+		bool start_change, end_change, mat_cord_change, start_color_change, end_color_change, width_change;
+		void default_num();
+		public:
+		object_change_line();
+		object_change_line(object_line *in_object_line);//?
+		object_line *body_object_line;//?
+		void set_start(vec3 in_start);
+		void set_end(vec3 in_end);
+		void set_mat_cord(vec3 in_mat_cord);
+		void set_start_color(color4 in_start_color);
+		void set_end_color(color4 in_end_color);
+		void set_width(GLint in_width);
+		void set_upper_object(object *in_object);
+
+		virtual void apply();
+		};
+
+
+}
+
+
+
+
+
+
+
 #endif
