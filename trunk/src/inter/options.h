e2c5987a132ad760011762485d609391ad774721
stalkerg <stalkerg@02b043cb-f327-0410-be5f-3fa6dc1791a0>
2007-04-24 21:51:03 +0000

    New swig bind. For Python and Ruby.

--- trunk/src/inter/options.h
+++ trunk/src/inter/options.h
@@ -2,9 +2,10 @@
 #define I_OPTIONS
 
 void set_options(unsigned short win_w, unsigned short win_h,
-			 unsigned short bit, bool double_buffer, bool fullscreen);
+			 unsigned short bit, short double_buffer, short fullscreen);
 void set_caption(char *name, char *long_name);
 void *init_cycle();
+void *get_null();
 
 #endif
 
