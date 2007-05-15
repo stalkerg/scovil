5f51ab71e94031de53a94ef9ea3a183be35b4322
stalkerg <stalkerg@02b043cb-f327-0410-be5f-3fa6dc1791a0>
2007-05-15 21:50:30 +0000

    fix fix fix... and more bug bug bug

--- trunk/src/examples/python/example3.py
+++ trunk/src/examples/python/example3.py
@@ -3,11 +3,15 @@ import pyscovil, time
 curent_options = pyscovil.options(640, 480, 32, False, True, "PyExample3", "Test")
 scene_object = pyscovil.scene()
 
+camera1 = pyscovil.camera()
+#camera1.look_at([0,0,1], [0,0,-1], [0,1,0])
+
 line1 = pyscovil.line(scene_object, [0,0,-1], [1,0,-1])
 triangle1 = pyscovil.triangle(line1, [0, 0.3, -1], [-0.3, 0, -1], [0.3, 0, -1])
 triangle1.set_color1([1,0,0,1])
 triangle1.set_color2([0,1,0,1])
 triangle1.set_color3([0,0,1,1], True)
+#camera1.set_current(True)
 for i in xrange(90):
 	line1.set_direction([0,0,1,i], True)
 	time.sleep(0.5)
