046aa20fbfb49dca3607daa0621f5b503f2fbc75
stalkerg <stalkerg@02b043cb-f327-0410-be5f-3fa6dc1791a0>
2007-05-15 17:05:03 +0000

    small fix

--- trunk/src/inter/change_triangle.h
+++ trunk/src/inter/change_triangle.h
@@ -6,7 +6,7 @@ void set_cord1_change_triangle(float x1, float y1, float z1);
 void set_cord2_change_triangle(float x2, float y2, float z2);
 void set_cord3_change_triangle(float x3, float y3, float z3);
 void set_mat_cord_change_triangle(float x, float y, float z);
-void set_direction_change_line(float x, float y, float z, float angle);
+void set_direction_change_triangle(float x, float y, float z, float angle);
 void set_color1_change_triangle(float r, float g, float b, float a);
 void set_color2_change_triangle(float r, float g, float b, float a);
 void set_color3_change_triangle(float r, float g, float b, float a);
